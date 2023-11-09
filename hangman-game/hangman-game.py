14eb727a7d7f0e5f0bfa210713d538a68aa8c122
ricardobar96 <ricardobar96@gmail.com>
2023-11-09 14:04:31 +0000

    Defined function to select random word and count its letters

--- hangman-game/hangman-game.py
+++ hangman-game/hangman-game.py
@@ -1,3 +1,14 @@
 from random import choice
 
 words = ['MOUNTAIN', 'HURRICANE', 'EARTHQUAKE', 'BREAKFAST']
+
+successes = []
+failures = []
+lives = 7
+end_game = false
+
+def random_word(words):
+    word = choice(words)
+    letters = len(set(word))
+
+    return word, letters
